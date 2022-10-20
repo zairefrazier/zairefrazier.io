1c7de71490e45c87f0d90e5049731cfb31641ae3
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-10-20 17:08:47 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const App= () => {
+  return (
+    <div className="App">
+      <div class="conatiner max-w-screen-sm mx-auto">
+          <div ><a href="/work"><p class="hover:text-red-600">HOME</p></a></div>
+          <div><a href="/work"><p class="hover:text-red-600">ABOUT</p></a></div>
+          <div><a href="/work"><p class="hover:text-red-600">PROJECTS</p></a></div>
+        </div>
+          <section class="hero container max-w-screen-sm mx-auto">
+      
+        <img class="mx-auto" src="src/IMG_7618 2.jpg" alt="screenshot" />
+        <p class="text-align: left"> zaire frazier</p>
+      </section>
+      
+      
+    </div>
+  )
+}
+
+export default App
