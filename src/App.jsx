f006e7ae49a9641f66f2dfd1c9dbdb8cba86f343
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-01-31 13:28:56 -0500

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const App= () => {
   return (
+    <Router>
     <div className="App">
       <div class="conatiner max-w-screen-sm mx-auto">
-          <div ><a href="/work"><p class="hover:text-red-600">HOME</p></a></div>
-          <div><a href="/work"><p class="hover:text-red-600">ABOUT</p></a></div>
-          <div><a href="/work"><p class="hover:text-red-600">PROJECTS</p></a></div>
+          <div ><a href="/"><p class="hover:text-red-600">HOME</p></a></div>
+          <div><a href="/about"><p class="hover:text-red-600">ABOUT</p></a></div>
+          <div><a href="/projects"><p class="hover:text-red-600">PROJECTS</p></a></div>
         </div>
           <section class="hero container max-w-screen-sm mx-auto">
       
@@ -14,6 +15,7 @@ const App= () => {
         <p class="text-align: left"> zaire frazier</p>
       </section>
     </div>
+    </Router>
   )
 }
 export default App
